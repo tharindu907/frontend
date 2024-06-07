eba512a541d86f00454cdb73522108a296909e0d
tharindu907 <tsandamalamarathunga@gmail.com>
2024-06-08 00:58:24 +0530

    shop page created

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Hero.css'
+import hand_icon from '../Assets/hand_icon.png'
+import arrow_icon from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_1.jpg'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className="hero-left">
+        <h2>Revamp Your Style with Our Exclusive Frocks!</h2>
+        <div>
+            <div className="hand-hand-icon">
+                <p>Discover</p>
+                {/*<img src={hand_icon} alt="" />*/}
+            </div>
+            <p>the Latest Trends</p>
+            <p>for Everyone</p>
+        </div>
+        <div className="hero-latest-btn">
+            <div>New Arrivals</div>
+            <img src={arrow_icon} alt="" />
+        </div>
+        <div>
+            <h3>Enjoy Free Shipping on Orders Over Rs 7500</h3>
+            <h3>Hassle-Free Exchanges and Returns on All Orders.</h3>
+            <h3>Island-Wide Delivery and Convenient Warehouse Pickups Available.</h3>
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={hero_image} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+    const [menu,setMenu] = useState("shop")
+
+  return (
+    <div className='navbar'>
+        <div className="nav-logo">
+           <img src={logo} alt="" />
+           <p>FurDeer</p>
+        </div>
+        <ul className="nav-menu">
+            <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none', color: '#696969' }} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("womens")}}><Link style={{ textDecoration: 'none', color: '#696969' }} to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none', color: '#696969' }} to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("occations")}}><Link style={{ textDecoration: 'none', color: '#696969' }} to='/occations'>Occation</Link>{menu==="occations"?<hr/>:<></>}</li>
+        </ul>
+        <div className="nav-login-cart">
+            <Link style={{textDecoration: 'none' }} to='/login'><button>Login</button></Link>
+            <Link style={{textDecoration: 'none' }} to='/cart'><img src={cart_icon} alt="" /></Link>
+            <div className="nav-cart-count">0</div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
