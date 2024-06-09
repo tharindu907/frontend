62d1eef6562b74c3ab066a43500933a2a4547de3
tharindu907 <tsandamalamarathunga@gmail.com>
2024-06-09 15:14:03 +0530

    offer component added. new collection component added. newsletter box component added.

---
+++ src/Components/Offers/Offers.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Offers.css'
+import exclusive_image from '../Assets/hero_2.png'
+
+const Offers = () => {
+  return (
+    <div className='offers'>
+      <div className="offers-left">
+        <h1>Special Deals</h1>
+        <h2>Just For You</h2>
+        <p>Enjoy Amazing Discounts on Our Top-Selling Items</p>
+        <button>Check Now</button>
+      </div>
+      <div className="offers-right">
+        <img className='offers-img' src={exclusive_image} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Offers
