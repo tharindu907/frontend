666a89cdd2da8013ab40cc91f929661f9adde3ea
tharindu907 <tsandamalamarathunga@gmail.com>
2024-06-10 16:27:41 +0530

    Fix product prop error in Breadcrum component and handle undefined product in Product component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import Product from './Pages/Product';
 import LoginSignup from './Pages/LoginSignup';
 import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
-import women_banner from './Components/Assets/banner_women.png'
-import kids_banner from './Components/Assets/banner_kids.png'
-import occations_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_4.png'
+import kids_banner from './Components/Assets/banner_4.png'
+import occations_banner from './Components/Assets/banner_4.png'
 
 function App() {
   return (
